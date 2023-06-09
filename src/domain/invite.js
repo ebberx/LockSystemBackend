5ee1ad3c49bbae48d3f522a1e369b036ef4296d2
ChrisOtt0 <sophusott@gmail.com>
2023-06-09 15:55:44 +0200

    lock_id mini refactor

--- src/domain/invite.js
+++ src/domain/invite.js
@@ -4,7 +4,7 @@ const { ObjectId } = require('mongodb');
 const inviteSchema = new Schema({
     from: ObjectId,
     to: ObjectId,
-    lock: ObjectId,
+    lock_id: ObjectId,
     date: Date,
     accepted: Boolean
 })
